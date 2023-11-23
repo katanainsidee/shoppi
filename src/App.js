327dfec1864357fc3f31e6768c393de0bc3c6361
katanainsidee <whoyuster@gmail.com>
2023-11-23 19:36:04 +0300

    Добавление компанента Categories, добавление массива с категориями (несколько категорий), вывод категорий на страницу с добавленными стилями, отображение товаров по категориям

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Items from "./components/Items";
+import Categories from "./components/Categories";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       orders: [],
+      currentItems: [],
       items: [
         {
           id: 1,
@@ -51,17 +53,31 @@ class App extends React.Component {
         },
       ],
     };
+    this.state.currentItems = this.state.items;
     this.addToOrder = this.addToOrder.bind(this);
     this.deleteOrder = this.deleteOrder.bind(this);
+    this.chooseCategory = this.chooseCategory.bind(this);
   }
   render() {
     return (
       <div className="wrapper">
         <Header orders={this.state.orders} onDelete={this.deleteOrder} />
-        <Items items={this.state.items} onAdd={this.addToOrder} />
+        <Categories chooseCategory={this.chooseCategory} />
+        <Items items={this.state.currentItems} onAdd={this.addToOrder} />
         <Footer />
       </div>
     );
+  }
+
+  chooseCategory(category) {
+    if (category === "all") {
+      this.setState({ currentItems: this.state.items });
+      return;
+    }
+
+    this.setState({
+      currentItems: this.state.items.filter((el) => el.category === category),
+    });
   }
 
   deleteOrder(id) {
---
+++ src/components/Categories.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+export class Categories extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [
+        {
+          key: "all",
+          name: "Всё",
+        },
+        {
+          key: "shoes",
+          name: "Обувь",
+        },
+        {
+          key: "outerwear",
+          name: "Верхняя одежда",
+        },
+        {
+          key: "pants",
+          name: "Штаны",
+        },
+        {
+          key: "accessories",
+          name: "Аксесуары",
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="categories">
+        {this.state.categories.map((el) => (
+          <div key={el.key} onClick={() => this.props.chooseCategory(el.key)}>
+            {el.name}
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Categories;
